87357b69d16df0a09ee2a576dba03f5f4400110d
Denis <d.b.shadrin@urfu.ru>
2021-11-15 10:12:12 +0500

    refactor

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PIL import Image
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def convert_image_to_mosaic(image, size, gradation_step):
+    for x in range(0, len(image), size):
+        for y in range(0, len(image[0]), size):
+            image[x:x + size, y:y + size] = get_average_brightness(
+                image[x:x + size, y:y + size], size, gradation_step)
+    return image
 
 
-# Press the green button in the gutter to run the script.
+def get_average_brightness(block, size, gradation_step):
+    average_color = (block[:size, :size].sum() / 3) // size ** 2
+    return int(average_color // gradation_step) * gradation_step
+
+
+def main():
+    image_file = Image.open(input("Введите имя файла, которое хотите конвертировать: "))
+    block_size = int(input("Введите размер блока: "))
+    gradations_count = int(input("Введите количество градаций серого: "))
+    image = np.array(image_file)
+    gradation_step = 255 // gradations_count
+
+    res = Image.fromarray(convert_image_to_mosaic(image, block_size, gradation_step))
+    res.save(input("Введите имя файла, в которой хотите сохранить результат: "))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
